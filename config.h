29c22e4b8ebb621c5e39e9d749244daf041f8d70
GNUAaryn <90221167+GNUAaryn@users.noreply.github.com>
2023-08-09 09:31:25 -0500

    Update config.h

--- config.h
+++ config.h
@@ -8,13 +8,13 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
-#include "/home/aaron/.cache/wal/colors-wal-dmenu.h"
-//static const char *colors[SchemeLast][2] = {
+//#include "/home/aaron/.cache/wal/colors-wal-dmenu.h"
+static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-//	[SchemeNorm] = { "#bbbbbb", "#222222" },
-//	[SchemeSel] = { "#eeeeee", "#005577" },
-//	[SchemeOut] = { "#000000", "#00ffff" },
-//};
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
