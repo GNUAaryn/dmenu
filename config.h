c3c47223ba069134f4c3ce49edd07d9e566a83ff
GNUAaryn <90221167+GNUAaryn@users.noreply.github.com>
2023-08-15 06:05:25 -0500

    Update config.h (drac time!!!)

--- config.h
+++ config.h
@@ -11,9 +11,9 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 //#include "/home/aaron/.cache/wal/colors-wal-dmenu.h"
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+        [SchemeNorm] = { "#bbbbbb", "#282a36" },
+        [SchemeSel] = { "#eeeeee", "#bd93f9" },
+        [SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
